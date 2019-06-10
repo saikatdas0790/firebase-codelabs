ad9bf2d91aac0eebbe78b72f232c16cea476421c
Saikat Das <saikatdas0790@gmail.com>
2019-06-10 18:16:47 +0530

    feat: show notifications

--- friendlychat-web/public/scripts/main.js
+++ friendlychat-web/public/scripts/main.js
@@ -139,13 +139,39 @@ function saveImageMessage(file) {
 
 // Saves the messaging device token to the datastore.
 function saveMessagingDeviceToken() {
-  // TODO 10: Save the device token in the realtime datastore
+  firebase
+    .messaging()
+    .getToken()
+    .then(currentToken => {
+      if (currentToken) {
+        console.log("Got FCM device token:", currentToken);
+        // Saving the Device Token to the datastore.
+        firebase
+          .firestore()
+          .collection("fcmTokens")
+          .doc(currentToken)
+          .set({ uid: firebase.auth().currentUser.uid });
+      } else {
+        // Need to request permissions to show notifications.
+        requestNotificationsPermissions();
+      }
+    })
+    .catch(error => console.log("Unable to get messaging token.", error));
 }
 
 // Requests permissions to show notifications.
 function requestNotificationsPermissions() {
-  // TODO 11: Request permissions to send notifications.
+  console.log("Requesting notifications permission...");
+  firebase
+    .messaging()
+    .requestPermission()
+    .then(() => saveMessagingDeviceToken())
+    .catch(error =>
+      console.error("Unable to get permission to notify.", error)
+    );
 }
+// Device Token
+// cxvF4PbLiYg:APA91bHHdIMlhRDBCwfZIhxAqZmGOu8ugLl2ExW4WlUEyWIs6Lt7s7tF0Y0K9_rMa854lIk2wyyEsGCKM5aDjXIgv-BgoVdlSQ0yuaR7Ke1syEmlJaFSTVhIOQs8eO2osj6g1Nxvg1Kl
 
 // Triggered when a file is selected via the media picker.
 function onMediaFileSelected(event) {
